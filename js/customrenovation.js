5e28c76eb35409283fc1f720639f60f8f5b40024
Travis Butterfield <travisbutterfield@gmail.com>
2021-07-02 16:11:24 -0700

    initial commit

---
+++ js/customrenovation.js
@@ -0,0 +1,21 @@
+/**
+ * @file
+ * customrenovation behaviors.
+ */
+
+(function ($, Drupal) {
+
+  'use strict';
+
+  /**
+   * Behavior description.
+   */
+  Drupal.behaviors.customrenovation = {
+    attach: function (context, settings) {
+
+      console.log('It works!');
+
+    }
+  };
+
+} (jQuery, Drupal));
